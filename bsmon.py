f13f9dfe41f99fabd82ab11d607bf3a207d82bbf
Allen Austin <aaustin@gmail.com>
2020-10-06 16:54:03 -0700

    fixed missing else in if statement; added syslogging

--- bsmon.py
+++ bsmon.py
@@ -1,14 +1,13 @@
 from blinkstick import blinkstick
-import psutil
+import psutil, syslog
 
 bstick = blinkstick.find_first()
 
 if bstick is None:
-    print ("No BlinkSticks found...")
-else:
-    print ("Displaying CPU usage (Green = 0%, Amber = 50%, Red = 100%)")
-    print ("Press Ctrl+C to exit")
-    
+    syslog.syslog(syslog.LOG_ERR, 'Blinkstick not found, exiting...')
+else:    
+    syslog.syslog('Blinkstick found, entering monitoring loop.')
+
     #go into a forever loop
     while True:
         cpu = psutil.cpu_percent(interval=1)
@@ -18,3 +17,5 @@ else:
 
         bstick.set_color(index=0,red=cpu_intensity,green=255 - cpu_intensity,blue=0)
         bstick.set_color(index=1,red=mem_intensity,green=255 - mem_intensity,blue=0)
+
+syslog.syslog("Exiting normally.")
